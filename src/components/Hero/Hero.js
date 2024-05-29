8ee7b57d07608e276086bc84264e186b7f42935a
adryanzx <adryan_freire@hotmail.com>
2024-05-29 17:03:43 -0300

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Hero.css'; 
+import bulletsIcon from '../assets/bullets.svg';
+import thumbnailGif from '../assets/thumbnail.gif';
+import playIcon from '../assets/play.svg';
+import faixaEtaria from '../assets/faixa-etaria.png';
+
+function HeroSection() {
+  return (
+    <section className="s-hero">
+      <div className="container">
+        <div className="left" data-aos="fade-right">
+          <h3 className="subtitle">
+            <img src={bulletsIcon} alt="Bullets" />
+            <span>Lançamento dia 09/06/2024</span>
+          </h3>
+          <h1>God of War<br /> Ragnarök</h1>
+          <p>Os maiores heróis dos nove reinos, lutando por um futuro em que acreditam.</p>
+          <div className="value">
+            <strong>R$ 349,00</strong>
+            <a href="#" target="_blank">Comprar na pré-venda</a>
+          </div>
+        </div>
+        <div className="right" data-aos="fade-up">
+          <span>Assista o trailer</span>
+          <button>
+            <img src={thumbnailGif} className="thumbnail" alt="thumbnail do trailer" />
+            <img src={playIcon} className="play" alt="Icon play" />
+          </button>
+          <div className="year-old">
+            <img src={faixaEtaria} alt="Faixa etária" />
+            <p>Drogas Lícitas, Linguagem Imprópria, Violência Extrema</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
